87cc2b9de4f1a3f7453dd23c46f3e3dad463e341
马美英 <mameiying@meituan.com>
2019-07-11 13:56:03 +0800

    refactor: 修改打包路径

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -12,7 +12,7 @@ let prodConfig = merge(baseWebpackConfig, {
     mode: 'production',
     devtool: 'hidden-source-map',
     output: {
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, '../testCI'),
         filename: '[name].[chunkhash:8].js',
         chunkFilename: '[id].[chunkhash:8].js'
     },
